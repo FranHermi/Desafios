16d016d9714283718ca645948ef81c205df277c5
FranHermi <130852874+FranHermi@users.noreply.github.com>
2023-12-28 15:24:57 -0300

    Update app.js

--- DESAFIOS/3er_desafio(1)/src/app.js
+++ DESAFIOS/3er_desafio(1)/src/app.js
@@ -2,7 +2,8 @@
 
 const express = require('express')
 const app = express()
-const products = require('./productManager.js')
+const ProductManager = require('./productManager.js')
+const products = new ProductManager()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -15,14 +16,14 @@ app.listen(port, ()=>{
 //http://localhost:8080/api/products
 app
     .get('/api/products', (req, res)=>{
-    let productList = products.products.readFileProducts()
+    let productList = products.readFileProducts()
     console.log(productList)
     res.send(productList)
 })
     .get('/api/products', (req, res)=>{
     const limit = req.query.parseInt(limit);
     if(limit){
-        let productList = products.products.readFileProducts()
+        let productList = products.readFileProducts()
         const limitedProducts = productList.slice(0, limit)
         res.send(limitedProducts)
     }
@@ -30,7 +31,7 @@ app
 //http://localhost:8080/api/products/:id
     .get('/api/products/:pid', (req,res)=>{
     const selectedProduct = ()=>{
-        let productList =  products.products.readFileProducts()
+        let productList =  products.readFileProducts()
         const {pid}=req.params
         productoSelecto = productList.filter(prod=>prod.id!==parseInt(pid))
         console.log(productoSelecto)
@@ -38,6 +39,11 @@ app
     }    
 })
 
+
+//http://localhost:8080/products
+//http://localhost:8080/products?limit=5
+//http://localhost:8080/products/2
+//http://localhost:8080/products/34123123
 
 //http://localhost:8080/products
 //http://localhost:8080/products?limit=5
